f72a7fda6c28e25077bdb5323a0ab6eafc570969
Luigi Scagnet <luigi.scagnet@gmail.com>
2012-04-16 18:25:32 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgWeb::Application.config.secret_token = 'a8ba36b95f7b6486f44d243a636284887c00b61e524970123afd57906014fce78c384aad3a43442a88a6d80b850449e8a43d6f5235f22ba7ea25493073b4fe56'
